ab1dd54f57a9c71539bc58fb9c63de635d45379c
Kudrot467 <kkudrotekhoda@gmail.com>
2023-12-22 18:26:37 +0600

    delete added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.clkkquk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +27,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
    const usersCollection= client.db('tasksDB').collection('users');
+   const tasksCollection= client.db('tasksDB').collection('tasks');
 
 
    app.post('/jwt',async(req,res)=>{
@@ -51,6 +52,31 @@ async function run() {
       next();
     })
   }
+
+
+   app.delete('/tasks/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id : new ObjectId(id)}
+        const result=await tasksCollection.deleteOne(query);
+        res.send(result)
+    })
+
+  app.get('/tasks',async(req,res)=>{
+    let query={};
+    if(req.query?.email){
+        query={email: req.query.email}
+    }
+    const cursor=tasksCollection.find(query);
+    const result=await cursor.toArray();
+    res.send(result);
+
+})
+
+app.post('/tasks',async(req,res)=>{
+    const post=req.body;
+    const result=await tasksCollection.insertOne(post);
+    res.send(result);
+})
 
 
    app.get('/users',async(req,res)=>{
